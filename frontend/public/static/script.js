a97df808ef69f2dfcd0159df22989298c9610092
flcsn <flacson98@gmail.com>
2022-02-07 20:20:46 +0800

    removed unused variables

--- frontend/public/static/script.js
+++ frontend/public/static/script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const notRegisteredBtn = document.querySelector('.not-registered-btn')
   const registrationForm = document.querySelector('.registration-form')
-  const landingPage = document.querySelector('.landing-page')
 
   notRegisteredBtn.addEventListener('click', () => {
     registrationForm.classList.toggle('hidden')
